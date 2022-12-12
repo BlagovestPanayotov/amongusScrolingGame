203a43356cc28cc6fd2ec2696418a22db08b17c7
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-12-12 08:32:09 +0000

    Adding functionality to press to start button

--- app/app.js
+++ app/app.js
@@ -1,2 +1,9 @@
-console.log('it works');
+const btn = document.querySelector('.startBtn');
+const gameField = document.querySelector('.gameScreen');
+btn.addEventListener('click', startGame);;
 
+
+function startGame() {
+    gameField.classList.remove('hidden');
+    btn.classList.add('hidden');
+}
