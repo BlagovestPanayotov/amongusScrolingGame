7387c256ad54eeebac28d949350e6c20690b768d
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-12-12 09:21:59 +0000

    Add game engine

--- app/main.js
+++ app/main.js
@@ -5,4 +5,6 @@ let game = initGameObjects();
 game.btn.addEventListener('click', () => {
     game.btn.classList.add('hidden');
     game.gameScreen.classList.remove('hidden');
+
+    start(state,game)
 });
