a6fdc2e27bc1e3ccf2a48919bc9e83adb49fb9d6
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-12-26 12:17:09 +0000

    Showing the hero

--- app/gameObjects.js
+++ app/gameObjects.js
@@ -1,11 +1,10 @@
-
-
 function initGameObjects(){
     const gameScreen = document.querySelector('.game-screen');
     const btn = document.querySelector('.start-button');
 
     return {
         gameScreen,
-        btn
+        btn,
+        // hero,
     }
 }
